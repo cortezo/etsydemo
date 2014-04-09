a298d31411be790cb20e33bfa69ee97a5576be86
Victor <vwalldev@gmail.com>
2014-04-08 20:12:36 -0700

    second commit
    Signed-off-by: Victor <vwalldev@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Etsydemo::Application.routes.draw do
+  get "pages/about"
+  get "pages/contact"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
